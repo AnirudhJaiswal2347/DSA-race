ac7b1b84978f7613c8623254a64bd3ddac5d17b3
Anirudh Jaiswal <anirudh.jaiswal2518@gmail.com>
2026-09-14 22:06:19 +0530

    Create Day01_TwoSum.java for Two Sum solution
    Added initial implementation of Two Sum problem.

---
+++ arrays/Day01_TwoSum.java
@@ -0,0 +1,23 @@
+/*
+ * 🏁 DSA RACE
+ *
+ * Day: 01
+ * Track: Arrays
+ * Lap: Two Sum
+ * Difficulty: Easy
+ *
+ * Result: ⏳
+ * Time: --
+ *
+ * Time Complexity: --
+ * Space Complexity: --
+ */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        // Your solution here
+
+        return new int[]{};
+    }
+}
