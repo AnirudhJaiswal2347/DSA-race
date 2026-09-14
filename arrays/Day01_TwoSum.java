6e12a94e29576a74225682c06dfdd79eddfcacdc
Anirudh Jaiswal <anirudh.jaiswal2518@gmail.com>
2026-09-14 22:23:46 +0530

    🏁 Day 01: Two Sum

--- arrays/Day01_TwoSum.java
+++ arrays/Day01_TwoSum.java
@@ -5,19 +5,23 @@
  * Track: Arrays
  * Lap: Two Sum
  * Difficulty: Easy
+ * Time: 09:52
+ * Result: Accepted ✅
  *
- * Result: ⏳
- * Time: --
- *
- * Time Complexity: --
- * Space Complexity: --
+ * Approach: Brute Force
+ * Time Complexity: O(n²)
+ * Space Complexity: O(1)
  */
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        // Your solution here
-
+        for (int i=0; i<nums.length; i++){
+            for (int j=i+1; j<nums.length; j++){
+                if (nums[i]+nums[j]==target){
+                    return new int[]{i, j};
+                }
+            }
+        }
         return new int[]{};
     }
 }
