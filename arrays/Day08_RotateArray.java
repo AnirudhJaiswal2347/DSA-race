16a1a4464e22be71e8841ae294fbbcc072f57862
Anirudh Jaiswal <anirudh.jaiswal2518@gmail.com>
2026-09-21 01:17:32 +0530

    Day_08

---
+++ arrays/Day08_RotateArray.java
@@ -0,0 +1,41 @@
+/*
+ * 🏁 DSA RACE
+ *
+ * Day: 08
+ * Track: Arrays
+ * Lap: Rotate Array
+ * LeetCode: #189
+ * Difficulty: Medium
+ * Result: Accepted ✅
+ *
+ * Approach: Three Reversals
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+
+        if (n == 0) {
+            return;
+        }
+
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
